03048acdce612698a0189e697a4616465c73f216
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-03 19:15:48 -0400

    render queried data from the database

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,23 +1,26 @@
-from flask import ( Blueprint, render_template, request, redirect ) 
-from . import models 
+from flask import Blueprint, render_template, request, redirect
+from . import models
 
-bp = Blueprint('fact', __name__, url_prefix="/facts")
+bp = Blueprint("fact", __name__, url_prefix="/facts")
 
-@bp.route('/', methods=['GET', 'POST'])
-def index(): 
-    if request.method == 'POST':
-        submitter = request.form['submitter']
-        fact = request.form['fact']
 
-        new_fact = models.Fact(submitter=submitter,fact=fact)
+@bp.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        submitter = request.form["submitter"]
+        fact = request.form["fact"]
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
         models.db.session.add(new_fact)
         models.db.session.commit()
 
-        # print(request.form)
         return redirect('/facts')
     
-    return render_template('facts/index.html')
+    results = models.Fact.query.all()
 
-@bp.route('/new')
-def new(): 
-    return render_template('facts/new.html')
+    return render_template('facts/index.html', facts=results)
+
+
+@bp.route("/new")
+def new():
+    return render_template("facts/new.html")
